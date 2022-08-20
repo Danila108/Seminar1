2abcc56a356336309dc6b39f1e4189d86aadfaa0
DanilaChikhman <danila.chihman2015@yandex.ru>
2022-08-20 08:29:33 +0600

    homework1.3

---
+++ HomaWork1.3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 6: Напишите программу,
+ которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет 
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if( number1 % 2 == 0)
+{
+    System.Console.Write("чётное");
+}
+else{System.Console.WriteLine("нечетное");}
