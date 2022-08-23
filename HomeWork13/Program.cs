69638837d741890adff9fd8b2fbbcff89ac9f711
DanilaChikhman <danila.chihman2015@yandex.ru>
2022-08-23 12:13:30 +0600

    дз 13

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+System.Console.WriteLine("Введите xисло: ");
+String number = Convert.ToString(Console.ReadLine());
+int len = number.Length;
+
+if (len >2){
+    
+    System.Console.Write(number[2]);
+    }
+else{
+   System.Console.Write(" Ошибка! Нет третьего числа ");
+}
