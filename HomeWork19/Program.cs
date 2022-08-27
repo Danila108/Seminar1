4520864255495b766f2a7b1a5866d3b471127404
DanilaChikhman <danila.chihman2015@yandex.ru>
2022-08-27 18:33:15 +0600

    19

--- HomeWork19/Program.cs
+++ HomeWork19/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+            
+            System.Console.WriteLine("Введите 5-ти значное число: ");
+            String number = Convert.ToString(Console.ReadLine());
+            int len = number.Length;
+            
+            void N1(string number){
+                if (len != 5)
+                { Console.WriteLine("Ошибка ");}
+                else {
+          
+                    if (number[0]==number[len-1] && number[1]==number[len-2])
+                    {Console.WriteLine("Полиндром  ");}
+                    else {System.Console.WriteLine("не полиндром");}
+                    }
+                }
+
+            N1(number);
+       
+        
+    
