1b1f62e2dbaaea329da77c5ae6d958a29fdf55bf
DanilaChikhman <danila.chihman2015@yandex.ru>
2022-08-27 18:53:49 +0600

    home21

---
+++ HomWork21/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+double x1 = Kor("x", "A");
+double y1 = Kor("y", "A");
+double z1 = Kor("z", "A");
+double x2 = Kor("x", "B");
+double y2 = Kor("y", "B");
+double z2 = Kor("z", "B");
+
+int Kor(string KorName, string point)
+{
+    Console.Write($"Введите координату {KorName} точки {point}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+
+
--- HomeWork19/Program.cs
+++ HomeWork19/Program.cs
@@ -7,23 +7,24 @@
 23432 -> да
 */
 
-            
-            System.Console.WriteLine("Введите 5-ти значное число: ");
-            String number = Convert.ToString(Console.ReadLine());
-            int len = number.Length;
-            
-            void N1(string number){
-                if (len != 5)
-                { Console.WriteLine("Ошибка ");}
-                else {
-          
-                    if (number[0]==number[len-1] && number[1]==number[len-2])
-                    {Console.WriteLine("Полиндром  ");}
-                    else {System.Console.WriteLine("не полиндром");}
-                    }
-                }
 
-            N1(number);
-       
-        
-    
+System.Console.WriteLine("Введите 5-ти значное число: ");
+String number = Convert.ToString(Console.ReadLine());
+int len = number.Length;
+
+void N1(string number)
+{
+    if (len != 5)
+    { Console.WriteLine("Ошибка "); }
+    else
+    {
+
+        if (number[0] == number[len - 1] && number[1] == number[len - 2])
+        { Console.WriteLine("Полиндром  "); }
+        else { System.Console.WriteLine("не полиндром"); }
+    }
+}
+
+N1(number);
+
+
