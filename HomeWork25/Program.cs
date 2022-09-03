a5c03c50f376c8a432a391032b3da9a3dab3306e
DanilaChikhman <danila.chihman2015@yandex.ru>
2022-09-03 07:05:42 +0600

    25

--- HomeWork25/Program.cs
+++ HomeWork25/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int rank = Convert.ToInt32(Console.ReadLine());
+int answer =1;
+int Rank(int num, int rank){
+  for (int i = 1; i <= rank; i++){
+    answer = answer*num;
+  }
+  return answer;
+}
+
+int R = Rank(num, rank);
+Console.Write(R);
