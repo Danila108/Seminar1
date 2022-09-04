134c9d51cec7d835ef02ed14e519318926ef1918
DanilaChikhman <danila.chihman2015@yandex.ru>
2022-09-04 11:58:54 +0600

    36

--- HomeWork36/Program.cs
+++ HomeWork36/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+ Console.Write("Введите колличество элементов в массиве: ");
+ int [] numbers = new int[int.Parse(Console.ReadLine())];
+ int min = 1;
+ int max = 1000;
+
+
+void FillArray(int[] array, int min, int max){
+  for (int i = 0; i<array.Length; i++ ){
+    array[i] = new Random().Next(min, max);  //заполняет массив случайными числами
+    Console.WriteLine(array[i]);
+  }
+}
+
+int QuantityPositive(int[] array){
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ ){
+    if (i % 2 != 0)//проверяет на  нечётность позиции
+    {
+
+      quantity= quantity + array[i];
+    }
+  }
+  return quantity;  //возвращает сумму на нечетных позициях
+}
+
+FillArray(numbers, min, max);
+int quantity = QuantityPositive(numbers);
+Console.WriteLine($"Сумма на нечетных позициях: {quantity}");
