b2aaaea6dadbea712893f299c6612cbbf4b0f3dd
DanilaChikhman <danila.chihman2015@yandex.ru>
2022-09-17 11:51:21 +0600

    64 66 68

---
+++ HomeWork66/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+int sum = 0;
+if (M>N)
+for (int i = N; i <= M; i++)
+    sum = sum +i;
+else
+    for (int i = M; i <= N; i++)
+        sum = sum +i;
+
+Console.Write($" {sum}");
---
+++ HomeWork68/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+m = 2, n = 3 -> A(m,n) = 29 */
+
+Console.Write("Введи n: ");
+int n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи m: ");
+int m = Convert.ToInt16(Console.ReadLine());
+
+int functionAkkerman = Aker(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Aker(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Aker(m - 1, 1);
+  else return Aker(m - 1, Aker(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+
